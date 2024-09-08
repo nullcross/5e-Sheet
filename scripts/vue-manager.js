d6706dd4e3c632de0a10b98b9f36dae5d5f0f1c2
Null Cross <108197673+nullcross@users.noreply.github.com>
2024-09-08 18:17:14 -0400

    Dark theme and adjustments for print
    The colors have all been turned into custom properties, which change depending on the theme. As for the print adjustments, i removed the "screen" part of some media queries that I tend to add by force of habit, because without them, the grid gets funky. I imagine this site is the kind of site someone might actually want to print, given it's TTRPG related, so fixes like this do matter.

--- scripts/vue-manager.js
+++ scripts/vue-manager.js
@@ -1,10 +1,17 @@
-import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
-// import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js'
+import { createApp, ref, onMounted } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+// import { createApp, ref, onMounted } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js'
+
+const themes = [
+    { "index": 0, "className": "light", "displayName": "Light", "iconSrc": "media/alessio-atzeni-sun-icon.svg" },
+    { "index": 1, "className": "dark", "displayName": "Dark", "iconSrc": "media/fa-moon-solid.svg" },
+];
 
 // An array representing the universal stats of a character with a level equal to index.
 const levelStatKey = [
-    { "doesnt-exist": true, 
-        "exp": 0, "proficiencyBonus": 2 },    // 0 (duplicate of 1, to make indexing consistent with levels)
+    {
+        "doesnt-exist": true,
+        "exp": 0, "proficiencyBonus": 2
+    },    // 0 (duplicate of 1, to make indexing consistent with levels)
     { "exp": 0, "proficiencyBonus": 2 },    // 1
     { "exp": 300, "proficiencyBonus": 2 },    // 2
     { "exp": 900, "proficiencyBonus": 2 },    // 3
@@ -39,6 +46,8 @@ function parseIntFormatted(stringToParse)
 createApp({
     setup()
     {
+        const currentDisplayMode = ref(window.matchMedia("(prefers-color-scheme: dark)").matches ? themes[1] : themes[0]);
+
         const stats = ref({
             "characterName": "",
             "characterLevel": 0,
@@ -50,6 +59,33 @@ createApp({
             "characterExperience": "",
         });
 
+
+
+        function setTheme(e, t = "")
+        {
+            let targetMode;
+            // If t is a display mode object, just use it
+            if (t.iconSrc) targetMode = t;
+            // if not, try and find a matching mode (don't waste time on empty/whitespace)
+            else if (typeof t === "string" && !/^\s*$/.test(t))
+                targetMode = themes.find(el => el.className === t);
+
+
+            // If we've got an e with a valid currentTarget, use that, otherwise go fetch the button
+            let modeButton = (e ? e.currentTarget : null) ?? document.querySelector("#toggle-theme");
+            // If no target mode, take the mode directly after the current one (wrapping around with %)
+            targetMode ??= themes[(currentDisplayMode.value.index + 1) % themes.length];
+
+
+            document.documentElement.setAttribute("class", targetMode.className);
+
+            modeButton.setAttribute("title", `Theme: ${targetMode.displayName} - Click to switch`);
+            modeButton.setAttribute("aria-label", `Theme: ${targetMode.displayName} - Click to switch`);
+            modeButton.querySelector("img").setAttribute("src", targetMode.iconSrc);
+
+            modeButton.value = targetMode.index;
+            currentDisplayMode.value = targetMode;
+        }
 
 
         function applyChanges(e) { stats.value[e.target.name] = e.target.value; }
@@ -87,13 +123,20 @@ createApp({
 
 
 
+        onMounted(() =>
+        {
+            setTheme(null, currentDisplayMode.value);
+        })
+
         return {
             stats,
             levelStatKey,
 
+            setTheme,
             applyChanges,
             applyParsedChanges,
             sanitizeNumber
         }
     }
 }).mount('#app')
+
